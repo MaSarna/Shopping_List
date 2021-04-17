61cb1cb233052704251f085bd2f03586fb769b85
MaSarna <merix44@gmail.com>
2021-04-17 23:29:18 +0200

    Shopping list fun

--- app/src/main/java/com/curlybraces/shoppinglistm/data/db/ShoppingDao.kt
+++ app/src/main/java/com/curlybraces/shoppinglistm/data/db/ShoppingDao.kt
@@ -3,9 +3,16 @@ package com.curlybraces.shoppinglistm.data.db
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.curlybraces.shoppinglistm.data.db.entities.ShoppingItem
+import com.curlybraces.shoppinglistm.data.db.entities.ShoppingList
 
 @Dao
 interface ShoppingDao {
+
+    @Insert
+    suspend fun insertShoppingList(shoppingList: ShoppingList)
+
+    @Update
+    suspend fun updateShoppingList(shoppingList: ShoppingList)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(item: ShoppingItem)
@@ -13,6 +20,12 @@ interface ShoppingDao {
     @Delete
     suspend fun delete(item: ShoppingItem)
 
-    @Query("SELECT * FROM shopping_items")
-    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
+    @Query("SELECT * FROM shopping_list WHERE is_archived IS 0 ORDER BY shopping_list_timestamp DESC")
+    fun getCurrentShoppingList(): LiveData<List<ShoppingList>>
+
+    @Query("SELECT * FROM shopping_list WHERE is_archived IS 1 ORDER BY shopping_list_timestamp DESC")
+    fun getArchivedShoppingList(): LiveData<List<ShoppingList>>
+
+    @Query("SELECT * FROM shopping_items WHERE shopping_list_id = :listId ORDER BY item_timestamp DESC")
+    fun getAllShoppingItems(listId: Long): LiveData<List<ShoppingItem>>
 }

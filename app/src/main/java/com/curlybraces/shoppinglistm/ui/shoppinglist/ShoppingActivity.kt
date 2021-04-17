a6e68aa208c1a0d1231cc1a8d1bbfd804beda8e9
MaSarna <merix44@gmail.com>
2021-04-17 18:20:42 +0200

    setContentView

--- app/src/main/java/com/curlybraces/shoppinglistm/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/curlybraces/shoppinglistm/ui/shoppinglist/ShoppingActivity.kt
@@ -2,10 +2,15 @@ package com.curlybraces.shoppinglistm.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.curlybraces.shoppinglistm.R
 import com.curlybraces.shoppinglistm.data.db.ShoppingDatabase
+import com.curlybraces.shoppinglistm.data.db.entities.ShoppingItem
 import com.curlybraces.shoppinglistm.data.repositories.ShoppingRepository
+import com.curlybraces.shoppinglistm.other.ShoppingItemAdapter
+import kotlinx.android.synthetic.main.activity_shopping.*
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +22,24 @@ class ShoppingActivity : AppCompatActivity() {
         val factory  = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+
+        rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        rvShoppingItems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        fab.setOnClickListener {
+            AddShoppingItemDialog(this,
+                object : AddDialogListener {
+                    override fun onAddButtonClicked(item: ShoppingItem) {
+                        viewModel.upsert(item)
+                    }
+                }).show()
+        }
     }
 }

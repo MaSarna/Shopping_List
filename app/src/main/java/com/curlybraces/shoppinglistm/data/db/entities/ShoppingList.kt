22f5589202ecb8b9819d00e1fa48a664db82f4e9
MaSarna <merix44@gmail.com>
2021-04-17 23:01:06 +0200

    Shopping Lists Entity

---
+++ app/src/main/java/com/curlybraces/shoppinglistm/data/db/entities/ShoppingList.kt
@@ -0,0 +1,19 @@
+package com.curlybraces.shoppinglistm.data.db.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_list")
+class ShoppingList(
+    @ColumnInfo(name = "shopping_list_name")
+    var shoppingListName: String,
+    @ColumnInfo(name = "shopping_list_timestamp")
+    var shoppingListTimestamp: Long,
+    @ColumnInfo(name = "is_archived")
+    var isArchived: Boolean
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}

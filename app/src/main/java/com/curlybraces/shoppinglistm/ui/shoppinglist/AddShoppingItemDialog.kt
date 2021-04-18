90d0db3caa3cca845720804534aa6711c5fd0dcd
MaSarna <merix44@gmail.com>
2021-04-18 22:27:37 +0200

    Minior Changes

--- app/src/main/java/com/curlybraces/shoppinglistm/ui/shoppinglist/AddShoppingItemDialog.kt
+++ app/src/main/java/com/curlybraces/shoppinglistm/ui/shoppinglist/AddShoppingItemDialog.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialog
 import com.curlybraces.shoppinglistm.R
 import com.curlybraces.shoppinglistm.data.db.entities.ShoppingItem
+import com.curlybraces.shoppinglistm.data.db.entities.ShoppingList
 import kotlinx.android.synthetic.main.dialog_add_shopping_item.*
 
 class AddShoppingItemDialog(context: Context, var addDialogListener: AddDialogListener) : AppCompatDialog(context) {
@@ -17,13 +18,14 @@ class AddShoppingItemDialog(context: Context, var addDialogListener: AddDialogLi
         tvAdd.setOnClickListener {
             val name = etName.text.toString()
             val amount = etAmount.text.toString()
+            val timeStamp = System.currentTimeMillis()
 
             if(name.isEmpty() || amount.isEmpty()) {
                 Toast.makeText(context, "Please enter all the information", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            val item = ShoppingItem(name, amount.toInt())
+            val item = ShoppingItem(name, amount.toInt(), timeStamp)
             addDialogListener.onAddButtonClicked(item)
             dismiss()
         }

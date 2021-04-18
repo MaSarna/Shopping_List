b9447ac758b55df6b5c3fd7b38c28a9ebbc29270
MaSarna <merix44@gmail.com>
2021-04-18 16:09:54 +0200

    Ordering for items.

--- app/src/main/java/com/curlybraces/shoppinglistm/data/repositories/ShoppingRepository.kt
+++ app/src/main/java/com/curlybraces/shoppinglistm/data/repositories/ShoppingRepository.kt
@@ -2,10 +2,15 @@ package com.curlybraces.shoppinglistm.data.repositories
 
 import com.curlybraces.shoppinglistm.data.db.ShoppingDatabase
 import com.curlybraces.shoppinglistm.data.db.entities.ShoppingItem
+import com.curlybraces.shoppinglistm.data.db.entities.ShoppingList
 
 class ShoppingRepository(
     private val db: ShoppingDatabase
 ) {
+    suspend fun insertShoppingList(shoppingList: ShoppingList) = db.getShoppingDao().insertShoppingList(shoppingList)
+
+    suspend fun updateShoppingList(shoppingList: ShoppingList) = db.getShoppingDao().updateShoppingList(shoppingList)
+
     suspend fun upsert(item: ShoppingItem) = db.getShoppingDao().upsert(item)
 
     suspend fun delete(item: ShoppingItem) = db.getShoppingDao().delete(item)

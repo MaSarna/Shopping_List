58bb5f15d9a365a7dbd7d0fe174b12cda5f3031d
MaSarna <merix44@gmail.com>
2021-04-18 22:29:34 +0200

    Added List Methods to ViewModel

--- app/src/main/java/com/curlybraces/shoppinglistm/ui/shoppinglist/ShoppingViewModel.kt
+++ app/src/main/java/com/curlybraces/shoppinglistm/ui/shoppinglist/ShoppingViewModel.kt
@@ -2,6 +2,7 @@ package com.curlybraces.shoppinglistm.ui.shoppinglist
 
 import androidx.lifecycle.ViewModel
 import com.curlybraces.shoppinglistm.data.db.entities.ShoppingItem
+import com.curlybraces.shoppinglistm.data.db.entities.ShoppingList
 import com.curlybraces.shoppinglistm.data.repositories.ShoppingRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,7 +10,15 @@ import kotlinx.coroutines.launch
 
 class ShoppingViewModel(
     private val repository: ShoppingRepository
-): ViewModel() {
+) : ViewModel() {
+
+    fun insertShoppingList(shoppingList: ShoppingList) = CoroutineScope(Dispatchers.Main).launch {
+        repository.insertShoppingList(shoppingList)
+    }
+
+    fun updateShoppingList(shoppingList: ShoppingList) = CoroutineScope(Dispatchers.Main).launch {
+        repository.updateShoppingList(shoppingList)
+    }
 
     fun upsert(item: ShoppingItem) = CoroutineScope(Dispatchers.Main).launch {
         repository.upsert(item)
@@ -18,6 +27,10 @@ class ShoppingViewModel(
     fun delete(item: ShoppingItem) = CoroutineScope(Dispatchers.Main).launch {
         repository.delete(item)
     }
+
+    fun getCurrentShoppingList() = repository.getCurrentShoppingList()
+
+    fun getArchivedShoppingList() = repository.getArchivedShoppingList()
 
     fun getAllShoppingItems() = repository.getAllShoppingItems()
 }
